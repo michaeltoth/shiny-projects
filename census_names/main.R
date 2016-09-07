fa842f007ed038ee4113c9e8091b5fe4d2548c8b
Michael Toth <mt.toth@gmail.com>
2016-09-06 21:17:08 -0400

    Adding gganimate & updating code

--- census_names/main.R
+++ census_names/main.R
@@ -1,7 +1,7 @@
 library(dplyr)
 library(readr)
 library(ggplot2)
-setwd('~/dev/names')
+setwd('~/dev/shiny-projects/census_names/')
 
 # Get first year
 names <- read_csv('input/yob1880.txt', col_names = c('name', 'sex', 'count'), col_types = cols(col_character(), col_character(), col_number()))
@@ -17,8 +17,18 @@ for(year in seq(1881, 2015)) {
     names <- rbind(names, current) # Combine into single dataset
 }
 
-names <- names %>% group_by(year, sex) %>% mutate(rank = seq_along(name))
+# Perform Aggregations
+names <- names %>% 
+         group_by(year, sex) %>% 
+         mutate(rank = seq_along(name),
+                proportion = count / sum(count))
+
 top_10_each_year <- names %>% group_by(year, sex) %>% top_n(n = 10, wt = count)
 
-write.csv(top_10_each_year, '../input/top_10_each_year.csv', row.names = F)
+# Write results to be used by Shiny apps
+write.csv(top_10_each_year, 'input/top_10_each_year.csv', row.names = F)
 write.csv(names, 'input/all_names.csv', row.names = F)
+
+
+
+# Animation
