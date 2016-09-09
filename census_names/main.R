c081b9a20169112ce2875bf3cee366eb5ca236dd
Michael Toth <michael@orchardplatform.com>
2016-09-09 14:34:48 -0400

    Finalizing plots

--- census_names/main.R
+++ census_names/main.R
@@ -33,43 +33,54 @@ write.csv(top_10_each_year, 'input/top_10_each_year.csv', row.names = F)
 write.csv(names, 'input/all_names.csv', row.names = F)
 
 
-
-# Animation
+# Produce Animations
 p <- ggplot(top_10_each_year, aes(x = proportion, y = count)) +
     #geom_point(aes(cumulative = T), size = 4) +
     geom_point(color = '#cccccc', size = 4, alpha = .10) +
     geom_point(aes(color = sex, frame = year), size = 4) +
-    xlab('Proportion of Babies Born') +
+    ggtitle('Year:') +
+    xlab('\nProportion (by sex) of Babies Born') +
     ylab('Number of Babies Born') +
-    scale_color_manual(values = c('#ff99ff', '#66ccff')) +
+    scale_color_manual(name = '', values = c('#ff7f00', '#377eb8'), labels = c('Female', 'Male')) +
     scale_x_continuous(labels = percent) + 
+    scale_y_continuous(labels = comma) +
     theme_bw() +
     theme(panel.border = element_blank(),
           panel.grid = element_blank(),
-          legend.position = 'none',
-          axis.ticks = element_blank())
+          axis.ticks = element_blank(),
+          legend.key = element_blank(),
+          legend.position = 'bottom',
+          axis.text = element_text(size = 14),
+          axis.title = element_text(size = 16),
+          legend.text = element_text(size = 12))
 
-gg_animate(p, interval = 0.2, ani.width = 800, ani.height = 400)
+gg_animate(p, filename = 'yearly-birth-names-with-grey.gif', interval = 0.2, ani.width = 600, ani.height = 400)
+
 
 # Based on https://gist.github.com/thomasp85/c8e22be4628e4420d4f66bcc6c88ac87
 # Which created https://twitter.com/thomasp85/status/694905779539812352
-# TODO: Make this into a function for tothr
 anim <- lapply(1:10, function(i) {top_10_each_year$year <- top_10_each_year$year + i; top_10_each_year$fade <- 1 / (i + 2); top_10_each_year})
 top_10_each_year$fade <- 1
-top_10_each_year <- rbind(top_10_each_year, do.call(rbind, anim))
+top_10_with_fade <- rbind(top_10_each_year, do.call(rbind, anim))
+top_10_with_fade <- filter(top_10_with_fade, year <= 2015)
 
-top_10_each_year <- filter(top_10_each_year, year <= 2015)
-
-p2 <- ggplot(top_10_each_year, aes(x = proportion, y = count, color = sex, frame = year, alpha = fade)) +
+p2 <- ggplot(top_10_with_fade, aes(x = proportion, y = count, color = sex, frame = year, alpha = fade)) +
     geom_point(size = 4) +
-    xlab('Proportion of Babies Born') +
+    ggtitle('Year:') +
+    xlab('\nProportion (by sex) of Babies Born') +
     ylab('Number of Babies Born') +
-    scale_color_manual(values = c('#ff99ff', '#66ccff')) +
+    scale_color_manual(name = '', values = c('#ff7f00', '#377eb8'), labels = c('Female', 'Male')) +
     scale_x_continuous(labels = percent) + 
+    scale_y_continuous(labels = comma) +
+    scale_alpha(guide = 'none') + # Remove alpha legend from plot output
     theme_bw() +
     theme(panel.border = element_blank(),
           panel.grid = element_blank(),
-          legend.position = 'none',
-          axis.ticks = element_blank())
+          axis.ticks = element_blank(),
+          legend.key = element_blank(),
+          legend.position = 'bottom',
+          axis.text = element_text(size = 14),
+          axis.title = element_text(size = 16),
+          legend.text = element_text(size = 12))
 
-gg_animate(p2, interval = 0.2, ani.width = 800, ani.height = 400)
+gg_animate(p2, filename = 'yearly-birth-names-with-trails.gif', interval = 0.2, ani.width = 600, ani.height = 400)
